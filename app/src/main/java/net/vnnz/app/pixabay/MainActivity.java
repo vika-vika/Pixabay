68b4ae970e02e4e8a2083150e329013dd2d686c6
vika L <vika.lapsar@gmail.com>
2017-01-26 17:22:07 +0200

    Dialog added

--- app/src/main/java/net/vnnz/app/pixabay/MainActivity.java
+++ app/src/main/java/net/vnnz/app/pixabay/MainActivity.java
@@ -1,9 +1,13 @@
 package net.vnnz.app.pixabay;
 
+import android.app.Dialog;
 import android.content.res.Configuration;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -11,12 +15,17 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 
+import android.view.LayoutInflater;
 import android.view.View;
 
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import net.vnnz.app.pixabay.adapter.GridLayoutAdapter;
@@ -123,7 +132,57 @@ public class MainActivity extends AppCompatActivity implements RequestListener<S
     @Override
     public void onClick(View view) {
         Hits hits = (Hits) view.getTag();
-        Toast.makeText(this, hits.getId()+"", Toast.LENGTH_LONG).show();
+        AddTipDialogFrag frag = AddTipDialogFrag.newInstance(1, hits.getUser()+"", 0);
+        frag.show(getSupportFragmentManager(), "dialog");
+    }
+
+
+    public static class AddTipDialogFrag extends DialogFragment {
+
+        public static AddTipDialogFrag newInstance(int id, String title, int layoutId) {
+
+            AddTipDialogFrag frag = new AddTipDialogFrag();
+            Bundle args = new Bundle();
+            args.putString("title", title);
+            args.putInt("layoutId", layoutId);
+            args.putInt("dialogId", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            View dialogView = getActivity().getLayoutInflater().inflate(R.layout.dialog, null);
+
+            TextView text = (TextView) dialogView.findViewById(R.id.dialog_content_text);
+            text.setText("Are you sure you want to see image of " + getArguments().getString("title") + "?" );
+            Button positive = (Button) dialogView.findViewById(R.id.button_positive);
+            positive.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        //getActivityInstance().onPositiveClicked(getDialogId(), getData());
+                        dismiss();
+                }
+            });
+
+
+            Button negative = (Button) dialogView.findViewById(R.id.button_negative);
+            negative.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    dismiss();
+                }
+            });
+
+            Dialog dialog = new AlertDialog.Builder(getActivity())
+                    .setCustomTitle(null)
+                    .setView(dialogView)
+                    .setCancelable(true)
+                    .create();
+
+            return dialog;
+        }
     }
 
 }
